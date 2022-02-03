213bf222b6d3d9017c539b3bf0bbacbaed46ef0d
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2022-02-02 22:41:01 -0600

    use ets cache

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,9 +1,9 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
   test "spawns buckets", %{registry: registry} do
@@ -21,6 +21,7 @@ defmodule KV.RegistryTest do
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     Agent.stop(bucket)
+    _ = KV.Registry.create(registry, "bogus") # ensure message is processed
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
@@ -29,6 +30,7 @@ defmodule KV.RegistryTest do
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     Agent.stop(bucket, :shutdown)
+    _ = KV.Registry.create(registry, "bogus") # ensure message is processed
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
